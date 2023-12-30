91ee585b9a8264703e646668397aedfb3a0587e9
derblankehans <henning.dinkela@uol.de>
2023-12-30 18:53:02 +0100

    minor changes

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-// JavaScript functionality for the website
-
-// JavaScript functionality for Dark Mode / Light Mode
-
 document.addEventListener('DOMContentLoaded', function () {
     const darkModeBtn = document.getElementById('darkModeBtn');
     const lightModeBtn = document.getElementById('lightModeBtn');
-    const body = document.body;
+    const htmlElement = document.documentElement;
 
     // Event listener for Dark Mode button
     darkModeBtn.addEventListener('click', function () {
-        body.classList.add('dark-mode');
+        htmlElement.classList.add('dark-mode');
     });
 
     // Event listener for Light Mode button
     lightModeBtn.addEventListener('click', function () {
-        body.classList.remove('dark-mode');
+        htmlElement.classList.remove('dark-mode');
     });
 });
 
